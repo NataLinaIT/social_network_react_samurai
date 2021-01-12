c152799e2a191e5d1b148f47bed4f1f9b5562ab2
NataLina <kalipsoN80@gmail.com>
2021-01-12 21:34:54 +0200

    Updates

---
+++ precache-manifest.1ad28752b02df26952b4abf89dbd1427.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5ad7be4af10ce57ecde9f3c757eb56a6",
+    "url": "/social_network_react_samurai/index.html"
+  },
+  {
+    "revision": "c16d1bb4b6588e3ac06a",
+    "url": "/social_network_react_samurai/static/css/3.37a7c3d5.chunk.css"
+  },
+  {
+    "revision": "fa3eb92d7cb607e62699",
+    "url": "/social_network_react_samurai/static/css/4.47c589d7.chunk.css"
+  },
+  {
+    "revision": "6a33d246f9f5fe659429",
+    "url": "/social_network_react_samurai/static/css/main.327f72d5.chunk.css"
+  },
+  {
+    "revision": "a491a94391fb27347bf1",
+    "url": "/social_network_react_samurai/static/js/2.8c3cce71.chunk.js"
+  },
+  {
+    "revision": "29eb69a08ea4198f7b46e1db8a3d5045",
+    "url": "/social_network_react_samurai/static/js/2.8c3cce71.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c16d1bb4b6588e3ac06a",
+    "url": "/social_network_react_samurai/static/js/3.7f531568.chunk.js"
+  },
+  {
+    "revision": "fa3eb92d7cb607e62699",
+    "url": "/social_network_react_samurai/static/js/4.5eaa96d6.chunk.js"
+  },
+  {
+    "revision": "6a33d246f9f5fe659429",
+    "url": "/social_network_react_samurai/static/js/main.ba362d10.chunk.js"
+  },
+  {
+    "revision": "149502abdaf2891e4c7c",
+    "url": "/social_network_react_samurai/static/js/runtime-main.5d7c4c53.js"
+  },
+  {
+    "revision": "e8041637cdb27ad5d8ebe0d0fa086626",
+    "url": "/social_network_react_samurai/static/media/404.e8041637.png"
+  },
+  {
+    "revision": "c471e9f6019735584695055472a7881e",
+    "url": "/social_network_react_samurai/static/media/Elipse.c471e9f6.png"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/social_network_react_samurai/static/media/logo.5d5d9eef.svg"
+  },
+  {
+    "revision": "12dbfea20a8289a8a1e13e515c7e6925",
+    "url": "/social_network_react_samurai/static/media/profile-bg.12dbfea2.jpg"
+  },
+  {
+    "revision": "8c6822fda38b1e6e9277163340586383",
+    "url": "/social_network_react_samurai/static/media/user_default.8c6822fd.png"
+  }
+]);
